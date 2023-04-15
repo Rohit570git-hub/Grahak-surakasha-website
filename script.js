5778df3677359614da5aaad96c06ad3b00f96cb7
ROHIT KUMAR <rk15072000@gmail.com>
2023-04-15 11:55:55 +0530

    second commit

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const baseUrl = "https://grahaksuraksha-api.onrender.com"
+
+
+
 var responsiveSlider = function() {
 
     var slider = document.getElementById("slider");
@@ -54,7 +58,59 @@ var responsiveSlider = function() {
     responsiveSlider();  
     }
 
-    
-    
-    
+    let reportlist = document.getElementById('reportlist')
+    getAllReports()
+ //Get api call for all reports 
+function getAllReports() {
+  fetch(`${baseUrl}/report`, {
+    method: 'get',
+    headers: new Headers({'Content-Type': 'application/json'}),
+  })
+  .then(res => res.json())
+  .then(data=>{
+    console.log(data.reports)
+
+    //For Populating report list
+    let tableData = ""; 
+    data.reports.forEach(data => {
+      console.log("data elemet",data)
+      let fraudType = "";
+      if(data.fraud_type === 1){
+        fraudType = "Through Call"
+      }else if(data.fraud_type===2){
+        fraudType = "Messages"
+      }else if(data.fraud_type === 3){
+        fraudType ="UPI ID"
+      }
+          tableData +=`<tr>
+          <td>${fraudType}</td>
+          <td>${data.reported_entity}</td>
+          <td>${data.description}</td>
+          <td><img src ="placeholder.jpg" width="160" height="120"/></td>
+        </tr>`;
+       
+          // let report = document.createElement("div");
+          //   let rbutton=document.createElement("button");
+          //   let rpara = document.createElement("p");
+          
+          //   report.setAttribute("class", "report");
+          //   rbutton.setAttribute("class", "rbutton");
+          //   rpara.setAttribute("class", "rpara");
+
+             
+          //   bloodbank.appendChild(rpara)
+          //   bloodbank.appendChild(rbutton)
+          //   bloodbanklist.appendChild(report);
+
+          //   rpara.innerHTML=data;
+          //   rbutton.innerHTML="Click";
+      
+    });
+    document.getElementById("table_body").innerHTML = tableData;
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
+
+}      
     
